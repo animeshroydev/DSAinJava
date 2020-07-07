36aa9cd9f1cd7b1e274035a6af4e0c4fa47faaf2
animesh <royanimesh2211@gmail.com>
2020-07-07 12:05:45 +0530

    Vectors

---
+++ src/Lists/LinkedList/LinkedList.java
@@ -0,0 +1,15 @@
+package Lists.LinkedList;
+
+public class LinkedList {
+    public static void main(String[] args) {
+
+        int[] intArray = new int[5];
+        intArray[0] = 3;
+        intArray[1] = 4;
+        intArray[2] = 5;
+
+        for (int arr: intArray) {
+            System.out.println(arr);
+        }
+    }
+}
---
+++ src/Lists/SinglyLinkedList/Main.java
@@ -0,0 +1,28 @@
+package Lists.SinglyLinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Employee janeJones = new Employee("Jane", "John", 123);
+        Employee johnDoe = new Employee("John", "Doe", 4567);
+        Employee marrySmith = new Employee("Marry", "Smith", 22);
+        Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
+
+        EmployeeLinkedList list = new EmployeeLinkedList();
+
+        System.out.println(list.isEmpty());
+
+        list.addToFront(janeJones);
+        list.addToFront(johnDoe);
+        list.addToFront(marrySmith);
+        list.addToFront(mikeWilson);
+
+        System.out.println(list.isEmpty());
+
+        System.out.println(list.getSize());
+
+        list.printList();
+        
+    }
+}
+
