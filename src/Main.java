a66bcc569d4d756e043d97f3b2e42f94d6da1f58
animesh <royanimesh2211@gmail.com>
2020-07-07 19:47:03 +0530

    updated

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String s = "Your account balance is Rs 98";
-
-        if (s.matches("[0-9]")) {
-            System.out.println(s);
-        }
-    }
-}
