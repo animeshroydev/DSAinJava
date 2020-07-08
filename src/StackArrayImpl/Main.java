496f7469d4400822fcb702031f1b9ba4f26eadb5
animesh <royanimesh2211@gmail.com>
2020-07-08 12:45:26 +0530

    Stack Impl using Array

---
+++ src/StackArrayImpl/Main.java
@@ -0,0 +1,35 @@
+package StackArrayImpl;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayStack stack = new ArrayStack(10);
+
+        stack.push(new Employee("Jane", "Jones", 123));
+        stack.push(new Employee("John", "Doe", 4567));
+        stack.push(new Employee("Marry", "Smith", 22));
+        stack.push(new Employee("Mike", "Wilson", 3245));
+        stack.push(new Employee("Bill", "End", 78));
+
+        System.out.println("Size of the stack now: " + stack.getSize());
+
+        stack.printStack();
+
+        System.out.println("========= PEEK ==============");
+
+        System.out.println(stack.peek());
+
+        System.out.println("====== POPPED ========");
+
+        System.out.println("Popped: " + stack.pop());
+
+        System.out.println("==============");
+
+        stack.printStack();
+
+        System.out.println("==============");
+
+        System.out.println("Size of the stack now: " + stack.getSize());
+
+    }
+}
